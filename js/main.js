9813da77a35b3a1daf50c1bb8d5998ebbb4f9427
Victor Foster <vfoster@inspirato.com>
2016-05-08 22:50:17 -0600

    Added the initial harp setup, layout and styles

---
+++ js/main.js
@@ -0,0 +1,21 @@
+(function() {
+  var path = '//easy.myfonts.net/v2/js?sid=210860(font-family=Avenir+Next+Pro+Bold)&sid=217165(font-family=Avenir+Next+Pro)&sid=217166(font-family=Avenir+Next+Pro+Medium)&sid=217815(font-family=Avenir+Next+Pro+UltraLight)&key=kHVFZ43jMl',
+      protocol = ('https:' == document.location.protocol ? 'https:' : 'http:'),
+      trial = document.createElement('script');
+  trial.type = 'text/javascript';
+  trial.async = true;
+  trial.src = protocol + path;
+  var head = document.getElementsByTagName("head")[0];
+  head.appendChild(trial);
+})();
+
+(function() {
+  var path = '//easy.myfonts.net/v2/js?sid=176045(font-family=Freight+Text+Pro+Book)&sid=176050(font-family=Freight+Text+Pro+Light)&sid=176052(font-family=Freight+Text+Pro+Medium)&sid=176053(font-family=Freight+Text+Pro+SemiBold)&key=nqEzJAnleK',
+      protocol = ('https:' == document.location.protocol ? 'https:' : 'http:'),
+      trial = document.createElement('script');
+  trial.type = 'text/javascript';
+  trial.async = true;
+  trial.src = protocol + path;
+  var head = document.getElementsByTagName("head")[0];
+  head.appendChild(trial);
+})();
